7c1b1dcde51c696e290b152078f77bdf3cdd2223
Liubov <kortcova@gmail.com>
2022-08-07 19:07:09 +0300

    fetch

--- fetch_1.js
+++ fetch_1.js
@@ -1,40 +1,11 @@
-// fetch(`https://jsonplaceholder.typicode.com/posts`)
-//   .then((res) => {
-//     return res.json();
-//   })
-//   .then((posts) => console.log(posts));
-
-function getPost(id) {
-  return new Promise((resolve, reject) => {
-    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((post) => resolve(post))
-      .catch((err) => reject(err));
-  });
+async function getPost(id) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  );
+  const data = await response.json();
+  return data;
 }
 
-getPost(1).then((post) => console.log(post));
-
-const getPost2 = (id) => {
-  return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then(
-    (res) => {
-      return res.json();
-    }
-  );
-};
-
-getPost2(1)
-  .then((post) => console.log(post))
-  .then((err) => console.log(err));
-
-const getPost3 = (id) => {
-  return Promise.resolve().then(() => {
-    return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then(
-      (res) => {
-        return res.json();
-      }
-    );
-  });
-};
+getPost(1)
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
